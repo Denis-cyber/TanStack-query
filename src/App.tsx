0e1cb913103bb078b6f09ccbb04c9186bf62ea37
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-10-06 16:46:19 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+import { ITodo } from "./app.interface";
+
+const todoId = 1;
+
+function App() {
+  const { isLoading, data } = useQuery(
+    ["todos", todoId],
+    () => axios.get<ITodo>("https://jsonplaceholder.typicode.com/todos/1"),
+    { select: ({ data }) => data }
+  );
+
+  return (
+    <>
+      {isLoading && <div>Loading...</div>}
+
+      {data ? <h1>Todo: {data.title}</h1> : <h1>Data not found</h1>}
+    </>
+  );
+}
+
+export default App;
